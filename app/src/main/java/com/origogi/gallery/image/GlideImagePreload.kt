62e17fdcee8f82510c51c2999065814d88cfe64d
origogi <rlawjdxo1212@naver.com>
2021-04-29 16:14:20 +0900

    add Coroutine Scope

--- app/src/main/java/com/origogi/gallery/image/GlideImagePreload.kt
+++ app/src/main/java/com/origogi/gallery/image/GlideImagePreload.kt
@@ -2,16 +2,12 @@ package com.origogi.gallery.image
 
 import android.content.Context
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.engine.DiskCacheStrategy
 
 object GlideImagePreload {
-
 
     fun fetch(context: Context, url: String) {
         Glide.with(context).load(url)
             .preload(150, 150)
-        println("preload $url")
-
     }
 
 }
