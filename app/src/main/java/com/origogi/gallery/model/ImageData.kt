dc766b1841a924a506bc016a101a745cd6cd958c
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2021-05-26 13:17:27 +0900

    fix

--- app/src/main/java/com/origogi/gallery/model/ImageData.kt
+++ app/src/main/java/com/origogi/gallery/model/ImageData.kt
@@ -1,6 +1,3 @@
 package com.origogi.gallery.model
 
-sealed class Data
-
-data class ImageData(val imageUrl : String, val imageTitle : String) : Data()
-object EndData : Data()
+data class ImageData(val imageUrl : String, val imageTitle : String)
--- app/src/main/java/com/origogi/gallery/vm/MyViewModel.kt
+++ app/src/main/java/com/origogi/gallery/vm/MyViewModel.kt
@@ -1,14 +1,15 @@
 package com.origogi.gallery.vm
 
+import android.widget.TextView
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.origogi.gallery.model.EndData
 import com.origogi.gallery.model.ImageData
 import com.origogi.gallery.model.ImageDataProvider
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Dispatchers.Main
+import kotlinx.coroutines.channels.consumeEach
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
@@ -34,24 +35,16 @@ class MyViewModel : ViewModel() {
     fun load() {
 
         viewModelScope.launch(Dispatchers.IO) {
-            val channel = ImageDataProvider().get()
+            val channel = ImageDataProvider().get(viewModelScope)
 
             withContext(Main) {
                 counter.value = 0
             }
 
-            while (!channel.isClosedForReceive) {
-                when (val data = channel.receive()) {
-                    is ImageData -> {
-                        withContext(Main) {
-                            imageDataList.value = (imageDataList.value!! + data)
-                            counter.value = 1 + counter.value!!
-                        }
-                    }
-                    is EndData -> break
+            channel.consumeEach {
+                withContext(Main) {
+                    counter.value = counter.value!! + 1
                 }
-
-
             }
         }
     }
