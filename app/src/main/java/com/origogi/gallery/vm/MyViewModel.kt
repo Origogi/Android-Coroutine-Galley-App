49c229e566d2aeed8c6933be901686525ce45488
김정태(Jeongtae Kim)/App개발팀/11ST <origogi.kim@sk.com>
2021-05-28 14:29:09 +0900

    use livedata ktx

--- app/src/main/java/com/origogi/gallery/vm/MyViewModel.kt
+++ app/src/main/java/com/origogi/gallery/vm/MyViewModel.kt
@@ -1,10 +1,7 @@
 package com.origogi.gallery.vm
 
 import android.widget.TextView
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
 import com.origogi.gallery.model.ImageData
 import com.origogi.gallery.model.ImageDataProvider
 import kotlinx.coroutines.Dispatchers
@@ -15,43 +12,18 @@ import kotlinx.coroutines.withContext
 
 class MyViewModel : ViewModel() {
 
-    private val imageDataList: MutableLiveData<List<ImageData>> =
-        MutableLiveData<List<ImageData>>().apply {
-            viewModelScope.launch(Main) {
-                value = mutableListOf()
-            }
+    private val imageDataList = liveData {
+        val itemList = mutableListOf<ImageData>()
+        val channel = ImageDataProvider().get(viewModelScope)
+        channel.consumeEach {
+            itemList.add(it)
+            emit(itemList)
         }
 
-    private val counter: MutableLiveData<Int> = MutableLiveData<Int>().apply {
-        viewModelScope.launch(Main) {
-            value = 0
-        }
     }
 
-    init {
-        load()
-    }
-
-    fun load() {
-
-        viewModelScope.launch(Dispatchers.IO) {
-            val channel = ImageDataProvider().get(viewModelScope)
-
-            withContext(Main) {
-                counter.value = 0
-            }
-
-            channel.consumeEach { imageData ->
-                withContext(Main) {
-                    counter.value = counter.value?.let {
-                        it + 1
-                    }
-                    imageDataList.value = imageDataList.value ?.let {
-                        it + imageData
-                    }
-                }
-            }
-        }
+    private val counter = Transformations.map(imageDataList) {
+        it.size
     }
 
     fun getImageDataList() = imageDataList as LiveData<List<ImageData>>
